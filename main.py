59be4651264ff279485f4b2ff909288554699fae
oawajan <oawajan@uvm.edu>
2024-02-21 14:33:50 -0500

    test push

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
+def Project_name():
+    print(f'SUBREDDIT-COMMUNITIES-DYNAMIC-AND-NEWS-PROPAGATION')
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+    Project_name()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
